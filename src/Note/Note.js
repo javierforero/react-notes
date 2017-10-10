aa481d397cfa21064bbfdf0531d8cdf4afa37e7f
Javier Forero <javierforero01@gmail.com>
2017-10-09 23:04:42 -0500

    first commit

---
+++ src/Note/Note.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Note.css';
+import PropTypes from 'prop-types';
+
+class export Note extends Component {
+  constructor(props){
+    super(props);
+    this.message = "hello from the note Component";
+  }
+
+  render(props) {
+    return(
+      <div>
+       <h1>{this.message}</h1>
+      </div>
+    );
+  }
+}
